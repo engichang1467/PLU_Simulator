379ae4c74a7130ab565aea62a9b2324e0a824671
Michael Chang <mchang0926@gmail.com>
2019-08-25 17:09:24 -0700

    adding database helper function

--- plu_sim/Model.py
+++ plu_sim/Model.py
@@ -25,7 +25,46 @@ class PLUSimSchema(Schema):
     name = fields.String()
 
 class DatabaseHelper(object):
+
+    @staticmethod
+    def getPLUs():
+        return PLUSim.query.all()
+
+    @staticmethod
+    def getPLU(search):
+        return PLUSim.query.get(int(search))
+
+    @staticmethod
+    def convertArr(a, column):
+        l = []
+        if (column == "plu"):
+            for i in a:
+                l.append(i.plu)
+        elif (column == "name"):
+            for i in a:
+                l.append(i.name)
+        return l
     
     @staticmethod
-    def foo():
-        return
+    def addAllCSV(file):
+        plu = file['plu']
+        name = file['name']
+        for i in range(len(plu)):
+            e = PLUSim.query.get(int(plu[i]))
+            if (e == None):
+                a = PLUSim(plu=int(plu[i]), name=str(name[i]))
+                db.session.add(a)
+        db.session.commit()
+    
+    @staticmethod
+    def removePLU(search):
+        a = PLUSim.query.get(int(search))
+        db.session.delete(a)
+        db.session.commit()
+
+    @staticmethod
+    def deleteEverything():
+        a = PLUSim.query.all()
+        for i in a:
+            db.session.delete(i)
+        db.session.commit()
