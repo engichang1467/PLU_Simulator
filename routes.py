b12899ae08b80676c50a9fab3079f22ed41f95e5
Michael Chang <mchang0926@gmail.com>
2019-08-25 21:47:26 -0700

    adding comment on each function

--- routes.py
+++ routes.py
@@ -2,16 +2,19 @@
 import csv
 from forms import *
 from app import app
+from plu_sim.Model import *
 from testingPy.test import *
 from flask_login import current_user, login_user, logout_user, login_required
 from flask import render_template, flash, redirect,url_for, redirect, request, send_file
 
+# Desc: Home Page
 @app.route('/')
 @app.route('/home')
 def home():
     print("You are now on home page")
     return render_template('home.html', title='Welcome Home')
 
+# Desc: Upload page where user can upload file
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     print("Upload page is here")
@@ -29,3 +32,20 @@ def upload():
         flash("Database if updated")
 
     return render_template('upload.html', title="Upload Here")
+
+# Desc: searchPLU page where users can search PLU
+@app.route('/searchPLU', methods=['GET', 'POST'])
+def searchPLU():
+    print("Now you can search plu here!!!")
+
+    form = pluForm()
+
+    if (request.method == 'POST'):
+        plu = request.form['search']
+        # name = request.form['name']
+
+        result = PLUSim.query.get(int(plu))
+
+        flash("{}   {}".format(result.plu, result.name))
+    
+    return render_template('searchPLU.html', title='Search PLU', form=form)
