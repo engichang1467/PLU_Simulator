540736edab902fe38b90a0fba6f1059d0a86a6bd
Michael Chang <mchang0926@gmail.com>
2019-08-25 17:05:32 -0700

    the addEverything function is working

--- testingPy/test.py
+++ testingPy/test.py
@@ -1,2 +1,15 @@
 from app import db
 from plu_sim.Model import *
+
+
+class Everything(object):
+
+    @staticmethod
+    def addEverything(file):
+        DatabaseHelper().addAllCSV(file)
+        print("Database bas been updated")
+
+
+'''
+from testingPy.test import *
+'''
